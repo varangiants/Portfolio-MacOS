dc2d05a06f4acf651e8dc9071b97c59994ec3570
varangiants <hafis.akbar1459@gmail.com>
2026-09-24 17:14:08 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "#constants";
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="Apple logo" />
+        <p className="font-bold">Hafis Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt="{'icon-${id}'}" />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
